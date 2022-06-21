ecdcc3c25658c9d0a3155f7a2a09d8fbcdb5b30c
almirmurad <developer@webmurad.com.br>
2022-06-21 20:04:23 -0300

    adicionado normalize

--- assets/php/includes/head.php
+++ assets/php/includes/head.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/estilo.css">
     <link rel="stylesheet" href="assets/css/form.css">
+    <link rel="stylesheet" href="assets/css/normalize.css">
     <link rel="shortcut icon" href="assets/img/favicon.png">
     <link href="assets/fontawesome/css/all.css" rel="stylesheet"> <!--load all styles -->
     <!--Social Sharing -->
