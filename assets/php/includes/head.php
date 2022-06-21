04f3576594c3837dfc6461b356bd6b69d300ac5b
almirmurad <developer@webmurad.com.br>
2022-06-21 19:38:06 -0300

    social share

--- assets/php/includes/head.php
+++ assets/php/includes/head.php
@@ -8,6 +8,19 @@
     <link rel="stylesheet" href="assets/css/form.css">
     <link rel="shortcut icon" href="assets/img/favicon.png">
     <link href="assets/fontawesome/css/all.css" rel="stylesheet"> <!--load all styles -->
+    <!--Social Sharing -->
+    <meta property="og:url" content="https://www.webmurad.com.br"/>
+    <meta property="og:locale" content="pt_BR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Web Murad - Desenvolvimento Web" />
+    <meta property="og:description" content="Sites, blogs e sistemas para internet." />
+    <meta property="fb:profile_id" content="100000181477301" />
+    <meta property="og:image" content="https://www.webmurad.com.br/assets/img/ogFb.png" />
+    <meta property="og:image:secure_url" content="https://www.webmurad.com.br/assets/img/ogFb.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <!--End Social Sharing -->
      <!--adSense -->
      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1039888699751196"
      crossorigin="anonymous"></script>
